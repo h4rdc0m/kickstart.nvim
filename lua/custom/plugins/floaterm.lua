68b2813a62087d9f8fc5d8f7e9db0a0286306f33
Christiaan van Dam <chris@combat.nl>
2024-10-06 11:43:33 +0200

    Many config changes to support the Laravel ecosystem

---
+++ lua/custom/plugins/floaterm.lua
@@ -0,0 +1,9 @@
+return {
+  'voldikss/vim-floaterm',
+  keys = {
+    { '<leader>tn', ':FloatermNew<cr>' },
+    { '<leader>th', ':FloatermPrev<cr>' },
+    { '<leader>tl', ':FloatermNext<cr>' },
+    { '<leader>tt', ':FloatermToggle<cr>' },
+  },
+}
