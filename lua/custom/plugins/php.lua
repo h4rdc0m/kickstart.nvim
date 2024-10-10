c9e0a264375a37825a387837463ec16b5285b95d
Christiaan van Dam <chris@combat.nl>
2024-10-10 12:04:22 +0200

    Removed laravel plugin because it errors too much

--- lua/custom/plugins/php.lua
+++ lua/custom/plugins/php.lua
@@ -46,29 +46,6 @@ return {
     end,
   },
   {
-    -- Add the Laravel.nvim plugin which gives the ability to run Artisan commands
-    -- from nvim
-    'adalessa/laravel.nvim',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'tpope/vim-dotenv',
-      'MunifTanjim/nui.nvim',
-      'nvimtools/none-ls.nvim',
-    },
-    cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
-    keys = {
-      { '<leader>la', ':Laravel artisan<cr>' },
-      { '<leader>lr', ':Laravel routes<cr>' },
-      { '<leader>lm', ':Laravel related<cr>' },
-    },
-    event = { 'VeryLazy' },
-    config = true,
-    opts = {
-      lsp_server = 'intelliphense',
-      features = { null_ls = { enable = false } },
-    },
-  },
-  {
     -- Add the blade-nav.nvim plugin which provides Goto File capabilities
     -- for Blade files.
     'ricardoramirezr/blade-nav.nvim',
